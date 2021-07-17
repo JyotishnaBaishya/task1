d1b2fcc90a8c5352b8a1f735e9006966dfe7fabf
Jyotishna Baishya <jyotishna.b800@gmail.com>
2021-07-17 19:38:29 +0530

    default

--- doctor_patient_app/models.py
+++ doctor_patient_app/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
 	email = models.EmailField(unique=True, blank=False)
 	first_name=models.CharField(max_length=50, null=False, blank=False)
 	last_name=models.CharField(max_length=50 ,null=False, blank=False)
-	Profile_Image = models.ImageField(upload_to='uploads/', null=True, blank=True)
+	Profile_Image = models.ImageField(upload_to='uploads/', null=True, blank=True, default="uploads/default.png")
 	Address_Line_1 = models.CharField(max_length=100)
 	City=models.CharField(max_length=50)
 	State=models.CharField(max_length=50)
